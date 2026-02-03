4bc4abd89536cbb1eefef0b86b6e709b22cc7a64
zod <zeyadelkholy7@gmail.com>
2026-02-03 02:49:45 +0200

    adding finding longest match function body

--- lz77.cpp
+++ lz77.cpp
@@ -2,7 +2,26 @@
 #include <vector>
 bool LZ77::findLongestMatch(const std::vector<unsigned char>& data, int cursor, int& matchDist, int& matchLen)
 {
-
+  if ((cursor + 3)>= data.size()) {
+    return false;
+  }
+  int limit = std::max(0, cursor - WINDOW_SIZE);
+  // maybe optimize with hash chain later
+  for (int i = cursor - 1; i >= limit; --i) {
+    int length = 0;
+    while (length < MAX_MATCH_LENGTH &&
+           cursor + length < data.size() &&
+           data[i + length] == data[cursor + length]) {
+      ++length;
+    }
+    if (length >= 3 && length > matchLen) {
+      matchLen = length;
+      matchDist = cursor - i;
+      if (matchLen == MAX_MATCH_LENGTH) {
+        break;
+      }
+    }
+  }
 }
 LZ77::LZ77()
 {
